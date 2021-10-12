b8ed87dd587b8b5e658d4bf5af7207924bf98193
kelbalazs <64102143+kelbalazs@users.noreply.github.com>
2021-10-12 20:00:44 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+<table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>meta</th>
+          <th>title</th>
+          <th>is private</th> 
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.meta }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.is_private }}</td>
+        </tr>
+      </tbody>
+
+    </table>
+  
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items:''
+    }
+  },
+
+
+  created() {
+    axios.get(`https://zbeta2.mykuwaitnet.net/backend/en/api/v2/pages/`)
+    .then(response => {
+     
+      this.items = response.data.items
+    })
+    
+  }
+}
+
+
+
+</script>
+
+<style>
+
+
+ table, th, td {
+  
+  border: 1px solid black;
+ }
+th {
+  background-color:mediumseagreen;
+  color: white;
+}
+</style>
